020fc59b8117b9515ee8cfcf333daa4ea201eb74
ZhekaValek <eugenvalek0209@gmail.com>
2024-09-10 12:04:03 +0300

    gg

--- ClassTasks/Task 2.cpp
+++ ClassTasks/Task 2.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+int main(){
+    std::cout <<"Введите N";
+    int N;
+    std::cin>>N;
+    for (int i=1; i<=N; i++) {
+        std::cout <<i<<","<<i*i<<"\n";
+    }
+    return 0;
+}
