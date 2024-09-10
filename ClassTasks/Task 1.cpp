ff357ea0febbfc506edb14b261429cf8a5a49b0b
ZhekaValek <eugenvalek0209@gmail.com>
2024-09-10 11:44:14 +0300

    regulated tasks

--- ClassTasks/Task 1.cpp
+++ ClassTasks/Task 1.cpp
@@ -1,11 +1,30 @@
 //
-//  Task 1.cpp
+//  main.cpp
 //  ClassTasks
 //
 //  Created by Женя Валек on 09.09.2024.
 //
 
 #include <iostream>
-int main(){
+
+int main(int argc, const char * argv[]) {
+    int x,h,n,res;
+    std::cout << "Введите x ";
+    std::cin >> x;
+    std::cout << "Введите h ";
+    std::cin >> h;
+    std::cout << "Введите n ";
+    std::cin >> n;
+    for (int i=0; i<n; i++) {
+        res=x+(h*i);
+        if(res>=5){
+            res=2*res+3;
+        }
+        else{
+            res=x*(x+2);
+        }
+        std::cout<< res<<std::endl;
+    }
     
+    return 0;
 }
--- ClassTasks/Task 2.cpp
+++ ClassTasks/Task 2.cpp
@@ -1,8 +1,1 @@
-//
-//  Task 2.cpp
-//  ClassTasks
-//
-//  Created by Женя Валек on 09.09.2024.
-//
 
-#include <stdio.h>
