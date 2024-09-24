1dc3a993ca67d062b1585791b7a8a7eeab5b3bfb
ZhekaValek <eugenvalek0209@gmail.com>
2024-09-24 12:06:08 +0300

    5 задание 11 номер переделка

--- ClassTasks/main.cpp
+++ ClassTasks/main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 
-
+double polinom2(double f0,double f1,double f2,double n, double x){
+    double f,s2,per;
+    s2=f0+f1*x+f2*x*x;
+    per=x*x;
+    for (int i=3; i<=n; ++i) {
+        per*=x;
+        f=3*f2/f1+4*f0;
+        f0=f1;
+        f1=f2;
+        f2=f;
+        s2+=f2*per;
+    }
+    return s2;
+}
+double polinom1(double f0,double f1,double f2,double n, double x){
+    double s1,f;
+            s1=f0*x*x+f1*x+f2;
+            for (int i=3; i<=n; ++i) {
+                f=3*f2/f1+4*f0;
+                f0=f1;
+                f1=f2;
+                f2=f;
+                s1=s1*x+f2;
+                
+                
+            }
+            return s1;
+            
+        }
 int main()
 {
-    /*double n,x,s,d;
-    std::cout<<"Введите n: ";
-    std::cin>>n;
-    std::cout<<"Введите x: ";
-    std::cin>>x;
-    s=0;
-    d=x;
-    for (int i=1; i<=n; ++i) {
-        d=d*((-1)*x*x*(1.5-i))/(2*i*(2*i-1));
-        s=s+d;
-        std::cout<<d;
-    }
-    std::cout<<"Ответ: "<<s<<"\n";*/
     
     
-    double f0,f1,f2,f,n,s1,s2,x,per;
+    double f0,f1,f2,f,n,s1,s2,x,per,p1,p2;
     per=1;
     std::cout<<"Введите f0,f1,f2,n,x:";
     std::cin>>f0>>f1>>f2>>n>>x;
@@ -32,19 +47,11 @@ int main()
         std::cout<<f0*x*x+f1*x+f2<<" "<<f0+f1*x+f2*x*x<<"\n";
     }
     else{
-        s1=f0*x*x+f1*x+f2;
-        s2=f0/x/x+f1/x+f2;
-        per*=x*x;
-        for (int i=3; i<=n; ++i) {
-            f=3*f2/f1+4*f0;
-            f0=f1;
-            f1=f2;
-            f2=f;
-            s1=s1*x+f2;
-            per*=x;
-            s2=s2/x+f2;
-            
-        }
-        std::cout<<s1<<"  "<<s2*per<<"\n";}
+        p2=polinom2(f0, f1, f2, n, x);
+        p1=polinom1(f0, f1, f2, n, x);
+        std::cout<<p1<<" ";
+        std::cout<<p2<<"\n";
+    }
     return 0;
 }
+
