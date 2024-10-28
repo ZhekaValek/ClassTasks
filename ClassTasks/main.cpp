e69599ddc417a6d8355aef20ea5364f6a8ab3c39
ZhekaValek <eugenvalek0209@gmail.com>
2024-10-28 09:00:13 +0300

    последнее домашнее задание с последовательностью

--- ClassTasks/main.cpp
+++ ClassTasks/main.cpp
@@ -74,103 +74,225 @@ int main(){
 //решать задания в документах сортировка и строки выделенные зеленым цветом
 //выполнить за кол-во шагов равное или меньше n, где n -- размер одномерного массива внутри двумерного массива
 
-//void findRowTwoCicles(std::vector<std::vector<int>>& array){
-//    int res=-1;
+//int findRowOneCicles(std::vector<std::vector<int>>& array){
+//    int m=0;
 //    for (int i=0; i<array.size(); ++i) {
-//        if(array[0][i]==1 && array[i][0]==0){
-//            for (int j=1; j<array.size(); ++j) {
-//                if(!((array[j][i]==1&&array[i][j]==0)||(i==j))){
-//                    break;
-//                }
-//                if(j==array.size()-1){
-//                    res=i;
-//                }
-//            }
+//        if(array[m][i]!=0){
+//            m=i;
 //        }
 //    }
-//    if(res!=-1){
-//        std::cout<<res<<"\n";
-//    }else{
-//        std::cout<<"нет таких значений\n";
-//    }
 //    
-//    return;
+//    return m;
 //}
 //
 //int main(){
-//    std::vector<std::vector<int>> array={{2,1,1,1,0},
-//                                         {0,2,0,1,1},
-//                                         {0,1,2,1,1},
-//                                         {0,0,0,0,1},
+//    std::vector<std::vector<int>> array={{2,1,1,1,1},
+//                                         {0,2,1,1,1},
+//                                         {0,0,2,0,1},
+//                                         {0,0,1,0,1},
 //                                         {0,0,0,0,2}};
 //    
-//    ::findRowTwoCicles(array);
+//    std::cout<<::findRowOneCicles(array);
 //}
+//
 
 
 
+
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//// Функция для статического массива char
+//void remove_chars_static(char* s1, const char* s2) {
+//    int k=0;
+//    int c=0;
+//    int chars[256]={0};
+//    while(s2[c]!='\0'){
+//        chars[s2[c]]+=1;
+//        c++;
+//    }
+//    c=0;
+//    while(s1[c]!='\0'){
+//        if(chars[s1[c]]==0){
+//            s1[k]=s1[c];
+//            ++k;
+//            
+//        }
+//        ++c;
+//    }
+//    s1[k]='\0';
+//    for (int i=0; i<k; ++i) {
+//        std::cout<<s1[i];
+//    }
+//    std::cout<<"\n";
+//    
+//    
+//}
+//
+//// Функция для вектора<char>
+//void remove_chars_vector(std::vector<char>& v1, const char* s2) {
+//    v1.erase(std::remove_if(v1.begin(), v1.end(),
+//        [&s2](char c) { return std::find(s2, s2 + strlen(s2), c) != s2 + strlen(s2); }),
+//        v1.end());
+//}
+//
+//int main() {
+//    char static_array[] = "Hello World!";
+//    const char* static_s2 = "low";
+//
+//    std::vector<char> dynamic_vector(static_array, static_array + strlen(static_array));
+//    const char* dynamic_s2 = "Hello!";
+//
+//    // Применяем функцию к статическому массиву
+//    
+//
+//    // Применяем функцию к вектору
+//    remove_chars_vector(dynamic_vector, dynamic_s2);
+//
+//    std::cout << "Статический массив после удаления: ";
+//    remove_chars_static(static_array, static_s2);
+//    std::cout << "Динамический вектор после удаления: ";
+//    for (char c : dynamic_vector) {
+//        std::cout << c;
+//    }
+//    std::cout << std::endl;
+//
+//    return 0;
+//}
+//
+
+
+
+
+//
+//#include <iostream>
+//#include <sstream>
+//
+//using namespace std;
+
+//Задан текст, в котором между двумя элементами может быть много разных разделителей.
+//То же перед текстом и после текста.Необходимо получить список элементов этого
+//текста.Например, пусть множество символьных разделителей
+//следующее: ‘;’, ‘:’, ’ ‘(пробел), ‘\t’, ‘,’. Рассмотрим текст:
+//
+//     ,, :;abc ;;, as ds, 77;,:
+//
+//Тогда список элементов этого текста следующий:
+//
+//“abc”, “as”, “ds”, “77”
+//
+//Один из способов решения задачи(обратите внимание, что среди разделителей есть пробел;
+//если бы пробела не было среди разделителей, то способ решения был бы другой):
+//
+// -посимвольно просматриваем текст и заменяем разделитель на пробел;
+// -решаем задачу, для случая, когда разделителем является пробел.
+
+//int main()
+//  {
+//   char s[80] = ",,:;abc ;;, as ds, 77;,:";
+//   for (auto& x: s)
+//     {
+//      switch (x)
+//        {case ';':
+//         case ':':
+//         case '\t':
+//         case ',':
+//          x = ' ';
+//          break;
+//        }
+//     }
+//   cout << "\"" <<  &s[0] << "\"" << endl;
+//   istringstream str(s);
+//   string word;
+//    string row;
+//    str>>word;
+//    row+=word;
+//    while (str >> word){
+//        row+=" "+word;
+//        
+//    }
+//    std::cout<<row<<endl;
+//
+//   // Пусть теперь пробел не является разделителем. В этом случае для текста
+//   //   ,, :;abc ;;, as ds, 77;,:
+//   // результатом выделения слов будет следующий список слов:
+//   //   " ", "abc ", " as ds", " 77"
+//
+//   // 1. Заменяем разделитель на какой-то определенный разделитель, например ';'
+//   // 2. Решаем задачу для случая, когда разделителем является ';'
+//   {
+//    char s[80] = ",, :;abc ;;, as ds, 77;,:";
+//    for (auto& x : s)
+//      {
+//       switch (x)
+//         {
+//          case ';':
+//          case ':':
+//          case '\t':
+//          case ',':
+//            x = ';';
+//            break;
+//         }
+//      }
+//    cout << "\"" << &s[0] << "\"" << "\n" << endl;
+//    istringstream str(s);
+//    string word;
+//    getline(str, word, ';');
+//    while (getline(str, word, ';'))
+//      {if (!word.empty())
+//         cout << "\"" << word << "\"" << endl;
+//      }
+//   }
+//   return 0;
+//  }
+//
 
 
 
 
 #include <iostream>
-#include <cstring>
 
-// Функция для статического массива char
-void remove_chars_static(char* s1, const char* s2) {
-    int j = 0;  // Индекс для нового символа
-    int i = 0;  // Индекс для текущего символа
-
-    // Удаляем символы из s1
-    while (s1[i] != '\0') {
-        bool found = false;
-        for (const char* p = s2; *p != '\0'; p++) {
-            if (*p == s1[i]) {
-                found = true;
-                break;
+int main(){
+    
+    int k;
+    std::cout<<"Введите k:";
+    std::cin>>k;
+    int a[k];
+    for (int i=0; i<k; i++) {
+        a[i]=i+1;
+    }
+    int n;
+    std::cout<<"Введите n:";
+    std::cin>>n;
+    
+    int pos[n];
+    for (int i=0; i<n; i++) {
+        pos[i]=i;
+    }
+    
+    bool changed=false;
+    do{
+        changed=false;
+        for (int i=0; i<n; i++) {
+            std::cout<<a[pos[i]]<<" ";
+        }
+        std::cout<<"\n";
+        for (int i=1; i<n; i++) {
+            if(pos[i]-pos[i-1]==2){
+                pos[i-1]++;
+                changed=true;
             }
         }
-        
-        if (!found) {
-            s1[j++] = s1[i];  // Добавляем символ в новый массив
+        if(!changed){
+            pos[n-1]++;
         }
-        i++;
-    }
-    s1[j] = '\0';
-}
-
-// Функция для std::vector<char>
-void remove_chars_vector(std::vector<char>& v1, const std::string& s2) {
-    std::vector<char> temp;
+    }while(pos[n-1]<k);
     
-    for (char c : v1) {
-        if (std::find(s2.begin(), s2.end(), c) == s2.end()) {
-            temp.push_back(c);
-        }
-    }
-    
-    v1.assign(temp.begin(), temp.end());
-}
-
-int main() {
-    char static_array[] = "Hello World!";
-    const char* static_s2 = "l";
-
-    std::vector<char> dynamic_vector(static_array, static_array + strlen(static_array));
-    const std::string dynamic_s2 = "lo";
-
-    // Применяем функцию к статическому массиву
-    remove_chars_static(static_array, static_s2);
-
-    // Применяем функцию к вектору
-    remove_chars_vector(dynamic_vector, dynamic_s2);
-
-    std::cout << "Статический массив после удаления: " << static_array << std::endl;
-    std::cout << "Динамический вектор после удаления: ";
-    for (char c : dynamic_vector) {
-        std::cout << c;
-    }
-    std::cout << std::endl;
-
     return 0;
 }
+    
+
+
+
+
